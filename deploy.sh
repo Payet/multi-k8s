89cf215f0aff8b986753791a49789f4dfabf16ad
Bertrand Payet <bertrand@Bertrands-MBP.fritz.box>
2020-06-27 17:07:33 +0200

    Deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+
+docker build -t bpayet/multi-client:latest -t bpayet/multi-client:$GIT_SHA ./client/Dockerfile ./client
+docker build -t bpayet/multi-server:latest -t bpayet/multi-server:$GIT_SHA ./server/Dockerfile ./server
+docker build -t bpayet/multi-worker:latest -t bpayet/multi-worker:$GIT_SHA ./worker/Dockerfile ./worker
+
+docker push bpayet/multi-client:latest
+docker push bpayet/multi-server:latest
+docker push bpayet/multi-worker:latest
+
+docker push bpayet/multi-client:$GIT_SHA
+docker push bpayet/multi-server:$GIT_SHA
+docker push bpayet/multi-worker:$GIT_SHA
+
+kubectl apply -f ./k8s
+
+kubectl set image deployments/server-deployment server=bpayet/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment server=bpayet/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment server=bpayet/multi-worker:$GIT_SHA
